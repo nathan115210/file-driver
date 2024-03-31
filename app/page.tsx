e9cfd333e043b2c972dffd453b0c5b8fd8f28236
Hongyu Zhao <nathan_115210@hotmail.com>
2024-03-31 22:00:49 +0300

    refactor: move the upload dialog related code to a separate component

--- app/page.tsx
+++ app/page.tsx
@@ -1,104 +1,15 @@
 "use client";
-import { useOrganization, useUser } from "@clerk/nextjs";
-import { Button } from "@/components/ui/button";
-import { useMutation, useQuery } from "convex/react";
-import { api } from "@/convex/_generated/api";
-import { useEffect, useState } from "react";
-import {
-  Dialog,
-  DialogContent,
-  DialogDescription,
-  DialogHeader,
-  DialogTitle,
-  DialogTrigger,
-} from "@/components/ui/dialog";
-import { z } from "zod";
-import { zodResolver } from "@hookform/resolvers/zod";
-import { useForm } from "react-hook-form";
-import { Input } from "@/components/ui/input";
-import {
-  Form,
-  FormControl,
-  FormField,
-  FormItem,
-  FormLabel,
-  FormMessage,
-} from "@/components/ui/form";
-import { useToast } from "@/components/ui/use-toast";
-import { Loader2 } from "lucide-react";
 
-const formSchema = z.object({
-  title: z
-    .string()
-    .min(1, { message: "Title can not be empty." })
-    .max(200, { message: "Title must be less than 200 characters." }),
-  file:
-    typeof window === "undefined"
-      ? z.custom<FileList>(
-          (file) => file instanceof FileList,
-          "File is required.",
-        )
-      : z
-          .instanceof(FileList)
-          .refine((file) => file?.length == 1, "File is required."),
-});
+import { useQuery } from "convex/react";
+import { api } from "@/convex/_generated/api";
+import UploadButton from "@/components/UploadButton";
+import { useEffect, useState } from "react";
+import { useOrganization, useUser } from "@clerk/nextjs";
 
 export default function Home() {
   const [orgId, setOrgId] = useState<string | undefined>(undefined);
-  const [isFileDialogOpen, setIsFileDialogOpen] = useState<boolean>(false);
   const organization = useOrganization();
   const user = useUser();
-
-  const form = useForm<z.infer<typeof formSchema>>({
-    resolver: zodResolver(formSchema),
-    defaultValues: {
-      title: "",
-      file: undefined,
-    },
-  });
-  console.log("form", form);
-  const fileRef = form.register("file");
-  const generateUploadUrl = useMutation(api.files.generateUploadUrl);
-  const { toast } = useToast();
-
-  const fileOnSubmit = async (data: z.infer<typeof formSchema>) => {
-    if (!orgId) return;
-    //Get a short-lived upload URL
-    const postUrl = await generateUploadUrl();
-    // POST the file to the URL
-    const result = await fetch(postUrl, {
-      method: "POST",
-      headers: { "Content-Type": data.file[0].type },
-      body: data.file[0],
-    });
-    const { storageId: fileId } = await result.json();
-    // Save the newly allocated storage id to the database
-    try {
-      await createFile({ name: data.title, fileId, orgId }).then(() => {
-        form.reset();
-        setIsFileDialogOpen(false);
-        toast({
-          variant: "success",
-          title: "File uploaded",
-        });
-      });
-      form.reset();
-      setIsFileDialogOpen(false);
-      toast({
-        variant: "success",
-        title: "File uploaded",
-      });
-    } catch (err) {
-      console.error(err);
-      toast({
-        variant: "destructive",
-        title: "Uh oh! Something went wrong.",
-        description: "There was a problem with uploading file",
-      });
-      setIsFileDialogOpen(false);
-    }
-  };
-
   useEffect(() => {
     if (organization.isLoaded && user.isLoaded) {
       // if it's the personal account, use user's Id for orgId
@@ -106,83 +17,13 @@ export default function Home() {
     }
   }, [organization, user]);
 
-  const createFile = useMutation(api.files.createFile);
-
   const allFiles = useQuery(api.files.getAllFiles, orgId ? { orgId } : "skip");
 
   return (
     <main className={"container mx-auto pt-12 pb-12 max-h-full"}>
       <div className={"flex flex-wrap justify-between items-center"}>
         <h1 className={"text-4xl font-bold"}>Your files</h1>
-        {/*Upload file modal*/}
-        <Dialog
-          open={isFileDialogOpen}
-          onOpenChange={(isFileDialogOpen) => {
-            setIsFileDialogOpen(isFileDialogOpen);
-            /* reset the form after dialog is close*/
-            form.reset();
-          }}
-        >
-          <DialogTrigger asChild>
-            {/*//TODO: hide "upload button" while not log in*/}
-            <Button onClick={() => {}}>Upload file</Button>
-          </DialogTrigger>
-          <DialogContent>
-            <DialogHeader>
-              <DialogTitle className={"mb-8"}>Upload a file</DialogTitle>
-              <DialogDescription>
-                <Form {...form}>
-                  <form
-                    onSubmit={form.handleSubmit(fileOnSubmit)}
-                    className="space-y-8"
-                  >
-                    <FormField
-                      control={form.control}
-                      name="title"
-                      render={({ field }) => (
-                        <FormItem>
-                          <FormLabel>File Title</FormLabel>
-                          <FormControl>
-                            <Input placeholder="File title" {...field} />
-                          </FormControl>
-
-                          <FormMessage />
-                        </FormItem>
-                      )}
-                    />
-                    <FormField
-                      control={form.control}
-                      name="file"
-                      render={(field) => (
-                        <FormItem>
-                          <FormLabel>File</FormLabel>
-                          <FormControl>
-                            <Input
-                              type={"file"}
-                              placeholder="File"
-                              {...fileRef}
-                            />
-                          </FormControl>
-                          <FormMessage />
-                        </FormItem>
-                      )}
-                    />
-                    <Button
-                      type="submit"
-                      disabled={form.formState.isSubmitting}
-                      className={"flex gap-1"}
-                    >
-                      {form.formState.isSubmitting && (
-                        <Loader2 className={"mr-2 h-4 w-4 animate-spin"} />
-                      )}
-                      Submit
-                    </Button>
-                  </form>
-                </Form>
-              </DialogDescription>
-            </DialogHeader>
-          </DialogContent>
-        </Dialog>
+        <UploadButton orgId={orgId} />
       </div>
 
       {allFiles?.map((file) => {
