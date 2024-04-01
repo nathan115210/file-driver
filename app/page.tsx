1bf8ef2f526af22492cc74bdba711ad255fb14d0
Hongyu Zhao <nathan_115210@hotmail.com>
2024-04-01 04:46:03 +0300

    feat(page): add views for different stages

--- app/page.tsx
+++ app/page.tsx
@@ -6,6 +6,8 @@ import UploadButton from "@/components/UploadButton";
 import { useEffect, useState } from "react";
 import { useOrganization, useUser } from "@clerk/nextjs";
 import FileCard from "@/components/fileCard/FileCard";
+import Image from "next/image";
+import { Loader2 } from "lucide-react";
 
 export default function Home() {
   const [orgId, setOrgId] = useState<string | undefined>(undefined);
@@ -20,14 +22,48 @@ export default function Home() {
 
   const allFiles = useQuery(api.files.getAllFiles, orgId ? { orgId } : "skip");
 
+  if (!allFiles) {
+    return (
+      <main className={"container mx-auto pt-12 "}>
+        <div
+          className={
+            "flex flex-col gap-4 w-full items-center mt-12 text-gray-500"
+          }
+        >
+          <Loader2 className={"h-32 w-32 animate-spin"} />
+          <div className={"text-2xl"}>Loading...</div>
+        </div>
+      </main>
+    );
+  }
+  if (allFiles.length === 0) {
+    return (
+      <main className={"container mx-auto pt-12 "}>
+        <div className={"flex flex-col gap-4 w-full items-center mt-12"}>
+          <Image
+            className={"m-auto"}
+            src={"/emptyData.svg"}
+            alt={"empty data"}
+            width={"500"}
+            height={"500"}
+          />
+          <p className="text-pink-600 text-xl font-bold">
+            You have no files, go ahead ans upload one now!
+          </p>
+          <UploadButton orgId={orgId} />
+        </div>
+      </main>
+    );
+  }
+
   return (
-    <main className={"container mx-auto pt-12 pb-12 max-h-full"}>
+    <main className={"container mx-auto pt-12 "}>
       <div className={"flex flex-wrap justify-between items-center mb-8"}>
         <h1 className={"text-4xl font-bold"}>Your files</h1>
         <UploadButton orgId={orgId} />
       </div>
       <div className={"grid grid-cols-4 gap-4"}>
-        {allFiles?.map((file) => {
+        {allFiles.map((file) => {
           return <FileCard key={file._id} file={file} />;
         })}
       </div>
